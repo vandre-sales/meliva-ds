fd3ec885f9fff6f9bcb2d70c8a61a69d65ca0a05
Lea Verou <lea@verou.me>
2024-12-10 04:50:36 -0500

    [viewport-demp] Parts for zoom buttons, zoom cursors

--- src/components/viewport-demo/viewport-demo.ts
+++ src/components/viewport-demo/viewport-demo.ts
@@ -291,9 +291,13 @@ export default class WaViewportDemo extends WebAwesomeElement {
         <span part="controls">
           ${dimensions}
           <span class="zoom">
-            <wa-icon-button name="square-minus" variant="regular" @click=${() => this.zoomOut()}>-</wa-icon-button>
+            <wa-icon-button name="square-minus" variant="regular" @click=${() => this.zoomOut()} part="zoom-out button"
+              >-</wa-icon-button
+            >
             <span class="zoom-level"> ${Math.round(this.computedZoom * 100)}%</span>
-            <wa-icon-button name="square-plus" variant="regular" @click=${() => this.zoomIn()}>+</wa-icon-button>
+            <wa-icon-button name="square-plus" variant="regular" @click=${() => this.zoomIn()} part="zoom-in button"
+              >+</wa-icon-button
+            >
           </span>
         </span>
         <slot @slotchange=${this.handleSlotChange}></slot>
