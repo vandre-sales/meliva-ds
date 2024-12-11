19340adcfb0515f0f224c14c981913f61e05b6a4
konnorrogers <konnor5456@gmail.com>
2024-12-10 19:07:45 -0500

    fix turbo

--- docs/assets/examples/page-demo/demo.js
+++ docs/assets/examples/page-demo/demo.js
@@ -1,54 +1,43 @@
-async function run() {
-  await customElements.whenDefined('wa-checkbox');
-  let container = document.getElementById('page_slots_demo');
+await customElements.whenDefined('wa-checkbox');
+let container = document.getElementById('page_slots_demo');
 
-  if (!container) {
-    return;
-  }
+let fieldset = container.querySelector('fieldset');
+let iframe = container.querySelector('iframe');
+let stylesheets = Array.from(document.querySelectorAll("link[rel=stylesheet][href^='/dist/']"))
+  .map(i => i.outerHTML)
+  .join('\n');
+let includes = `${stylesheets}
+  <script src="/dist/webawesome.loader.js" type="module"></script>
+  <link rel="stylesheet" href="/assets/examples/page-demo/page.css">`;
 
-  let fieldset = container.querySelector('fieldset');
-  let iframe = container.querySelector('iframe');
-  let stylesheets = Array.from(document.querySelectorAll("link[rel=stylesheet][href^='/dist/']"))
-    .map(i => i.outerHTML)
+function render() {
+  let slots = Array.from(fieldset.querySelectorAll('wa-checkbox[name=slot]:is([data-wa-checked])'));
+  let slotsHTML = slots
+    .map(slot => {
+      let name = slot.getAttribute('value');
+      let description = slot.getAttribute('data-description');
+
+      let tag = 'div';
+      if (name.endsWith('header')) {
+        tag = 'header';
+      }
+      if (name.endsWith('footer')) {
+        tag = 'footer';
+      }
+
+      return `<${tag} class="slot-content" slot="${name}">
+		<strong>${name || 'main <em>(default)</em>'}</strong>
+		<p>${description}</p>
+	</${tag}>`;
+    })
     .join('\n');
-  let includes = `${stylesheets}
-    <script src="/dist/webawesome.loader.js" type="module"></script>
-    <link rel="stylesheet" href="/assets/examples/page-demo/page.css">`;
+  let page = iframe.contentDocument?.querySelector('wa-page');
 
-  function render() {
-    let slots = Array.from(fieldset.querySelectorAll('wa-checkbox[name=slot]:is([data-wa-checked])'));
-    let slotsHTML = slots
-      .map(slot => {
-        let name = slot.getAttribute('value');
-        let description = slot.getAttribute('data-description');
-
-        let tag = 'div';
-        if (name.endsWith('header')) {
-          tag = 'header';
-        }
-        if (name.endsWith('footer')) {
-          tag = 'footer';
-        }
-
-        return `<${tag} class="slot-content" slot="${name}">
-		  <strong>${name || 'main <em>(default)</em>'}</strong>
-		  <p>${description}</p>
-	  </${tag}>`;
-      })
-      .join('\n');
-    let page = iframe.contentDocument?.querySelector('wa-page');
-
-    if (page) {
-      page.innerHTML = slotsHTML;
-    } else {
-      iframe.srcdoc = `${includes}<wa-page>${slotsHTML}</wa-page>`;
-    }
+  if (page) {
+    page.innerHTML = slotsHTML;
+  } else {
+    iframe.srcdoc = `${includes}<wa-page>${slotsHTML}</wa-page>`;
   }
-  fieldset?.addEventListener('input', render);
-  render();
 }
-
-(async () => {
-  await run();
-  document.addEventListener('turbo:load', run);
-})();
+fieldset?.addEventListener('input', render);
+render();
