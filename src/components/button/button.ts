11d3a73f059600b1cb16be50064a88732771eac3
Lindsay M <126139086+lindsaym-fa@users.noreply.github.com>
2024-12-16 12:18:45 -0500

    Add native button styles and share with `<wa-button>` (#310)

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -8,6 +8,7 @@ import { WaInvalidEvent } from '../../events/invalid.js';
 import { MirrorValidator } from '../../internal/validators/mirror-validator.js';
 import { watch } from '../../internal/watch.js';
 import { WebAwesomeFormAssociatedElement } from '../../internal/webawesome-element.js';
+import nativeStyles from '../../styles/native/button.css';
 import { LocalizeController } from '../../utilities/localize.js';
 import '../icon/icon.js';
 import '../spinner/spinner.js';
@@ -53,7 +54,7 @@ import styles from './button.css';
  */
 @customElement('wa-button')
 export default class WaButton extends WebAwesomeFormAssociatedElement {
-  static shadowStyle = styles;
+  static shadowStyle = [nativeStyles, styles];
 
   static get validators() {
     return [...super.validators, MirrorValidator()];
@@ -264,7 +265,7 @@ export default class WaButton extends WebAwesomeFormAssociatedElement {
           'button--text': this.appearance === 'text',
           'button--pill': this.pill,
           'button--rtl': this.localize.dir() === 'rtl',
-          'button--visually-hidden-label': this.visuallyHiddenLabel,
+          'button--visually-hidden-label': this.visuallyHiddenLabel
         })}
         ?disabled=${ifDefined(isLink ? undefined : this.disabled)}
         type=${ifDefined(isLink ? undefined : this.type)}
