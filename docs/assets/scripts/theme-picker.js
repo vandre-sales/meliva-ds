971200cc88783abb86ed1f2a2af74df9ee7c26ae
Lea Verou <lea@verou.me>
2025-04-14 12:52:00 -0400

    Move `domChange()` to separate module (#868)

--- docs/assets/scripts/theme-picker.js
+++ docs/assets/scripts/theme-picker.js
@@ -1,34 +1,5 @@
-let initialPageLoadComplete = document.readyState === 'complete';
-
-if (!initialPageLoadComplete) {
-  window.addEventListener('load', () => {
-    initialPageLoadComplete = true;
-  });
-}
-
-// Helper for view transitions
-export function domChange(fn, { behavior = 'smooth', ignoreInitialLoad = true } = {}) {
-  const canUseViewTransitions =
-    document.startViewTransition && !window.matchMedia('(prefers-reduced-motion: reduce)').matches;
-
-  // Skip transitions on initial page load
-  if (!initialPageLoadComplete && ignoreInitialLoad) {
-    fn(false);
-    return null;
-  }
-
-  if (canUseViewTransitions && behavior === 'smooth') {
-    const transition = document.startViewTransition(() => {
-      fn(true);
-      // Wait a brief delay before finishing the transition to prevent jumpiness
-      return new Promise(resolve => setTimeout(resolve, 200));
-    });
-    return transition;
-  } else {
-    fn(false);
-    return null;
-  }
-}
+import { domChange } from './util/dom-change.js';
+export { domChange };
 
 export function nextFrame() {
   return new Promise(resolve => requestAnimationFrame(resolve));
