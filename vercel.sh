63ec9d5bc10cb388b6d5ed64540ec962e1bf594b
Konnor Rogers <konnor5456@gmail.com>
2025-06-10 13:59:52 -0400

    Try with PAT (#1049)
    * Try with PAT
    
    * Try with PAT

--- vercel.sh
+++ vercel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Running the build!"
-cd packages/webawesome && npm run build
+git clone "https://konnorrogers:$GITHUB_ACCESS_TOKEN@github.com/shoelace-style/webawesome-pro" packages/webawesome-pro
+cd packages/webawesome-pro && npm run build
