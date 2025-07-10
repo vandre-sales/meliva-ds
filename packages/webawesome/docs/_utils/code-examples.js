a1e879035c321e6124465cd5072d754310db461d
Lindsay M <126139086+lindsaym-fa@users.noreply.github.com>
2025-07-10 15:36:03 -0400

    Fix code example styles (#1156)

--- packages/webawesome/docs/_utils/code-examples.js
+++ packages/webawesome/docs/_utils/code-examples.js
@@ -37,7 +37,9 @@ export function codeExamplesPlugin(options = {}) {
         const codeExample = parse(`
           <div class="code-example ${isOpen ? 'open' : ''}">
             <div class="code-example-preview">
-              ${preview}
+              <div>
+                ${preview}
+              </div>
               <div class="code-example-resizer" aria-hidden="true">
                 <wa-icon name="grip-lines-vertical"></wa-icon>
               </div>
